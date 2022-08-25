056f2c8056db258c5cec6e1e74c78508f1799309
UXDESIGNERLIN <hochialing91@gmail.com>
2022-08-25 13:07:46 +0100

    Set up router for diary detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [
+  {
+    path: '/detail/:id',
+    name: 'DiaryDetail',
+    props: true,
+    component: () => import('../views/DiaryDetail.vue')
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
