0969e6a49b899e50449296d2849b98ae899c6cf4
UXDESIGNERLIN <hochialing91@gmail.com>
2022-08-26 05:24:42 +0100

    Added App

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,50 @@
   </div>
   <router-view/>
 </template>
+<style lang="scss">
+
+@import "./assets/scss/index.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+button {
+  border: 1px solid $theme-color;
+  border-radius: $border-radius;
+  background-color: transparent;
+  cursor: pointer;
+}
+input, textarea {
+  border: 2px solid $theme-color;
+  border-radius: 5px;
+  line-height: 2rem;
+  @include dark;
+}
+input::placeholder, textarea::placeholder {
+  @include dark;
+}
+a {
+  text-decoration: none;
+  color: $theme-color;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.FormFields {
+  width: 100%;
+}
+.error-field {
+  @include dark;
+}
+</style>
