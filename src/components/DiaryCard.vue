5115c012f884836ac5ceaac13bbcbb81cf2449d8
UXDESIGNERLIN <hochialing91@gmail.com>
2022-08-26 16:28:34 +0100

    Added remove button style

--- src/components/DiaryCard.vue
+++ src/components/DiaryCard.vue
@@ -4,9 +4,9 @@
       <img :src="card.imgurl" :alt="card.title"/>
     </div>
     <div class="DiaryCard__container__info">
-      <p>{{card.author}}</p>
       <p>{{card.title}}</p>
       <p class="DiaryCard__container__info__description">{{card.description}}</p>
+      <button @click.stop.prevent="deleteDiary">Remove</button>
     </div>
   </div>
 </template>
@@ -48,33 +48,56 @@ export default {
         object-fit: cover;
         object-position: 50% 50%;
         transition: .5s ease-in-out;
+        display:inline-block;
         @include fullWidthfullHeight;
       }
     }
     &__info {
+      max-height: 200px;
       @include in-the-middle;
-      &__description {
+      &__description, button {
         display: none;
       }
       p{
         margin: 0;
         font-size: 1rem;
         color: $theme-color-secondary;
+        @include truncate;
       }
     }
     &:hover img{
       transform: scale(1.5);
     }
     &:hover  &__info{
-      overflow: hidden;
       width: 80%;
       height: 50%;
-      &__description{
+      &__description {
         display: block;
+        height: 30%;
         font-size: 1rem;
         margin: 1rem;
       }
+      button {
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        margin: 50px 0;
+        color: $theme-color-secondary;
+        border: none;
+        z-index: 10;
+      }
     }
+    &:hover::after {
+    content:'';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    background-color: rgba(0,0,0,0.5);
+    @include in-the-middle;
+  }
   }
 }
 </style>
