c3c84319fd08501f7b09d563e95280b5a26e2066
UXDESIGNERLIN <hochialing91@gmail.com>
2022-08-28 07:24:46 +0100

    Added author field

---
+++ src/components/Form/DiaryAuthorField.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+    <input class="FormFields" type="text" placeholder="Diary Author"  v-model.trim="author" @input="$emit('update:modelValue', $event.target.value)" @keyup="validateInput" @blur="validateInput"/>
+    <div class="error-field" v-if="errors.author">
+      {{ errors.author }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import formValidators from '@/utils/formValidators'
+export default {
+  name: 'DiaryAuthorField',
+  emits: ['update:modelValue'],
+  setup () {
+    const author = ref('')
+    const { validateAuthor, errors } = formValidators()
+    const validateInput = () => {
+      validateAuthor('author', author.value)
+    }
+    return { author, errors, validateInput }
+  }
+}
+</script>
