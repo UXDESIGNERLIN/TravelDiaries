7a2470dbb5212232929c4cd09398bded78cc8b24
UXDESIGNERLIN <hochialing91@gmail.com>
2022-08-28 09:25:41 +0100

    Added field for modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,6 +2,8 @@
   <div class="Modal">
     <button @click="close" type="button">X</button>
     <form class="Modal__form" novalidate @submit.prevent>
+      <DiaryAuthorField v-model="diary.author"/>
+      <DiaryDescriptionField v-model="diary.description"/>
       <button type="submit" :disabled="isSubmitDisabled" @click.prevent="onSubmit">Submit</button>
     </form>
   </div>
@@ -13,23 +15,29 @@ import { v4 } from 'uuid'
 import { ref } from 'vue'
 import { useStore } from 'vuex'
 import router from '@/router'
+import DiaryAuthorField from '@/components/Form/DiaryAuthorField'
+import DiaryDescriptionField from '@/components/Form/DiaryDescriptionField'
 
 export default {
   name: 'Modal',
+  components: {
+    DiaryAuthorField,
+    DiaryDescriptionField
+  },
   emits: ['close'],
   setup (props, { emit }) {
+    const defaultImgurl = 'https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bGlmZXx8fHx8fDE2NjE2MjA4NDQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600'
     const store = useStore()
     const diary = ref({
       id: v4(),
-      title: '',
+      author: '',
       description: '',
-      author: 'hey',
-      imgurl: 'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/tomato.jpg'
+      imgurl: defaultImgurl
     })
+    const { isSubmitDisabled } = submitButtonState(diary.value)
     const close = () => {
       emit('close')
     }
-    const { isSubmitDisabled } = submitButtonState(diary.value)
     const onSubmit = () => {
       store.dispatch('postDiary', diary.value).catch((error) => {
         router.push({
@@ -52,7 +60,7 @@ export default {
   display: flex;
   position: fixed;
   justify-content: flex-end;
-  @include in-the-middle;
+  @include fixed-center;
   background-color: rgba(0,0,0,0.9);
   overflow: auto;
   &__form {
@@ -60,7 +68,7 @@ export default {
     flex-direction: column;
     justify-content: center;
     position: fixed;
-    @include in-the-middle;
+    @include relative-center;
   }
   &__form > div {
     padding: 10px;
